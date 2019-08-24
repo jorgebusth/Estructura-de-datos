151c89b122652f794a2fb274e50e14c54a70ff5a
jorgebusth <jbusth@gmail.com>
2019-08-24 18:01:21 -0500

    Create pr01.c
    Ordena los números de menor a mayor ocupando los extremos hacia el centro del arreglo

---
+++ pr01.c
@@ -0,0 +1,50 @@
+﻿#define MAX 10
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+
+	int A[MAX], i, j, x, y, posicion, m, temp, pm;
+
+	for(i=0;i<MAX;i++){
+	
+		printf(“Introduce el valor %d”,i+1);
+		scanf(“%d”, & A[i]);
+	}
+	
+	x=0, y=MAX-1;
+
+	for(i=0;i<MAX-1;i++){
+		
+		if(i%2==0){
+		posicion=x;
+		}
+		else
+		posicion=y;
+
+		m=A[x];
+
+		for( pm=x, j=x; j<=y; j++){
+
+		if(A[j]<A[x]){
+				m=A[j];
+				pm=j;
+			}		
+		}
+	}
+	
+	
+	temp=A[posicion];
+	A[posicion]=A[pm];
+	A[pm]=temp;
+	
+	if(posicion==x) 
+		x++;
+	else 
+		y--;
+
+	print(“\n El arreglo queda asi: \n”);
+
+	for(i=0 ; i<MAX ; i++)
+
+}
